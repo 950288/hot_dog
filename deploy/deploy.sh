96ce0d3e99b6404d75b3da45cc7fafec9ec74603
950288 <950288s@gmail.com>
2025-03-16 10:30:25 +0800

    update main.yml, Dockerfile and deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,5 +4,5 @@ sudo docker build -t hot_dog .
 echo "Deploying the application..."
 sudo docker stop hot_dog_container
 
-# sudo docker run -it --rm --name hot_dog_container -p 35545:80 hot_dog ./server
+# sudo docker run -it --rm --name hot_dog_container -p 35545:80 hot_dog ./web/server
 sudo docker run -d --rm --name hot_dog_container -p 35545:80 hot_dog ./server
